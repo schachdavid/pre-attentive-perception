c6a958af17785eb1886d155bbc30b4a70b7079a9
David Schach <schachdavid@web.de>
2020-04-30 13:13:05 +0200

    Updates

---
+++ precache-manifest.8165c6cd6ac465aa678a841b43ebe6b3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b1af87c8f83dce29021f21119b493430",
+    "url": "/pre-attentive-perception/index.html"
+  },
+  {
+    "revision": "6c4b737c9f2d2de2c0a1",
+    "url": "/pre-attentive-perception/static/css/2.c2395193.chunk.css"
+  },
+  {
+    "revision": "304cfb8cc3baf5f0598f",
+    "url": "/pre-attentive-perception/static/css/main.276fb89f.chunk.css"
+  },
+  {
+    "revision": "6c4b737c9f2d2de2c0a1",
+    "url": "/pre-attentive-perception/static/js/2.e9c3794a.chunk.js"
+  },
+  {
+    "revision": "bf74d9caf22e23642aac46d0c3b7ebb6",
+    "url": "/pre-attentive-perception/static/js/2.e9c3794a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "304cfb8cc3baf5f0598f",
+    "url": "/pre-attentive-perception/static/js/main.13633368.chunk.js"
+  },
+  {
+    "revision": "8ded316e71ab24e63633",
+    "url": "/pre-attentive-perception/static/js/runtime-main.1d919941.js"
+  }
+]);
